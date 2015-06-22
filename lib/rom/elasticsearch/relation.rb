3f8fb100ee2e2b2dba8d4cff54fee5b7484ac50f
Yuri Artemev <i@artemeff.com>
2015-06-22 12:58:41 +0300

    add #search to relation

--- lib/rom/elasticsearch/relation.rb
+++ lib/rom/elasticsearch/relation.rb
@@ -3,6 +3,7 @@ require 'rom/relation'
 module ROM
   module Elasticsearch
     class Relation < ROM::Relation
+      forward :search
       forward :<<, :insert
       forward :get, :filter, :query_string, :query
     end
