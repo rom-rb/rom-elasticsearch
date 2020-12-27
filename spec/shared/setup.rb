1361ae109ff42e58921dd7efb920990441a4cad0
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2020-12-27 09:53:11 +0300

    Update specs to show the issue with frozen client

--- spec/shared/setup.rb
+++ spec/shared/setup.rb
@@ -1,7 +1,7 @@
 RSpec.shared_context 'setup' do
   let(:uri) { "http://127.0.0.1:9200" }
 
-  let(:conf) { ROM::Configuration.new(:elasticsearch, uri) }
+  let(:conf) { ROM::Configuration.new(:elasticsearch, client: Elasticsearch::Client.new(url: uri)) }
   let(:container) { ROM.container(conf) }
 
   let(:gateway) { conf.gateways[:default] }
