4de3b1e6636139671c2a82f47ec72479e22a7f6b
Dave Sullivan <dave@davesullivan.ca>
2021-08-10 14:02:07 -0400

    Added spec to test reading/writing of nested fields

--- spec/integration/rom/elasticsearch/relation/schema_spec.rb
+++ spec/integration/rom/elasticsearch/relation/schema_spec.rb
@@ -5,23 +5,56 @@ RSpec.describe ROM::Elasticsearch::Relation, ".schema" do
 
   include_context "setup"
 
-  before do
-    conf.relation(:users) do
-      schema(:users) do
-        attribute :id,   ROM::Elasticsearch::Types::ID
-        attribute :name, ROM::Elasticsearch::Types.Text, read: ROM::Types.Constructor(Symbol, &:to_sym)
+  context "read/write types" do
+    before do
+      conf.relation(:users) do
+        schema(:users) do
+          attribute :id,   ROM::Elasticsearch::Types::ID
+          attribute :name, ROM::Elasticsearch::Types.Text, read: ROM::Types.Constructor(Symbol, &:to_sym)
+        end
       end
+    end
+
+    it "defines read/write types" do
+      relation.create_index
+
+      relation.command(:create).call(id: 1, name: "Jane")
+
+      user = relation.get(1).one
+
+      expect(user[:id]).to be(1)
+      expect(user[:name]).to be(:Jane)
     end
   end
 
-  it "defines read/write types" do
-    relation.create_index
+  context "nested fields" do
+    before do
+      conf.relation(:users) do
+        schema(:users) do
+          attribute :id,   ROM::Elasticsearch::Types::ID
+          attribute :name, ROM::Elasticsearch::Types.Text
+          attribute :profile, ROM::Elasticsearch::Types.Nested do
+            attribute :email, ROM::Elasticsearch::Types.Text
+          end
+        end
+      end
+    end
 
-    relation.command(:create).call(id: 1, name: "Jane")
+    it "persists and reads nested types" do
+      relation.create_index
 
-    user = relation.get(1).one
+      relation.command(:create).call(
+        id:   1,
+        name: "Jane",
+        profile: {
+          email: "jane@sample.com"
+        }
+      )
 
-    expect(user[:id]).to be(1)
-    expect(user[:name]).to be(:Jane)
+      user = relation.get(1).one
+
+      expect(user[:id]).to be(1)
+      expect(user[:profile]).to eql({ "email" => "jane@sample.com" })
+    end
   end
 end
